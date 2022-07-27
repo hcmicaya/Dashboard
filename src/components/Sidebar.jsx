a3254cd4ed905edcba9baa8b49a1ff2aeed065e8
Hans Micaya <hcmicaya@gmail.com>
2022-07-26 22:04:15 -0400

    Theme fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,8 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-    const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+    const { activeMenu, setActiveMenu, screenSize, currentColor } =
+        useStateContext();
 
     const handleCloseSideBar = () => {
         if (activeMenu && screenSize <= 900) {
@@ -60,6 +61,11 @@ const Sidebar = () => {
                                     <NavLink
                                         to={`/${link.name}`}
                                         key={link.name}
+                                        style={({ isActive }) => ({
+                                            backgroundColor: isActive
+                                                ? currentColor
+                                                : "",
+                                        })}
                                         onClick={handleCloseSideBar}
                                         className={({ isActive }) =>
                                             isActive ? activeLink : normalLink
